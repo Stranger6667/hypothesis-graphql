640bbaa94c95afc12061838ace442ab5713d61dd
Dmitry Dygalo <dmitry@dygalo.dev>
2022-04-27 16:47:21 +0200

    chore: Expose `validate_scalar_strategy` in the public API

--- src/hypothesis_graphql/_strategies/validation.py
+++ src/hypothesis_graphql/_strategies/validation.py
@@ -5,7 +5,7 @@ from hypothesis import strategies as st
 from hypothesis.errors import InvalidArgument
 
 from ..cache import cached_build_schema
-from ..types import CustomScalars
+from ..types import CustomScalarStrategies
 
 
 def maybe_parse_schema(schema: Union[str, graphql.GraphQLSchema]) -> graphql.GraphQLSchema:
@@ -14,21 +14,25 @@ def maybe_parse_schema(schema: Union[str, graphql.GraphQLSchema]) -> graphql.Gra
     return schema
 
 
-def validate_fields(fields: Tuple[str, ...], available_fields: Dict[str, graphql.GraphQLField]) -> None:
-    if not fields:
+def validate_fields(fields_: Tuple[str, ...], available_fields: Dict[str, graphql.GraphQLField]) -> None:
+    if not fields_:
         raise ValueError("If you pass `fields`, it should not be empty")
-    invalid_fields = tuple(field for field in fields if field not in available_fields)
+    invalid_fields = tuple(field for field in fields_ if field not in available_fields)
     if invalid_fields:
         raise ValueError(f"Unknown fields: {', '.join(invalid_fields)}")
 
 
-def validate_custom_scalars(custom_scalars: CustomScalars) -> None:
-    assert isinstance(custom_scalars, dict)
-    for name, strategy in custom_scalars.items():
-        if not isinstance(name, str):
-            raise InvalidArgument(f"scalar name {name!r} must be a string")
-        if not isinstance(strategy, st.SearchStrategy):
-            raise InvalidArgument(
-                f"custom_scalars[{name!r}]={strategy!r} must be a Hypothesis "
-                "strategy which generates AST nodes matching this scalar."
-            )
+def validate_custom_scalars(strategies: CustomScalarStrategies) -> None:
+    assert isinstance(strategies, dict)
+    for name, strategy in strategies.items():
+        validate_scalar_strategy(name, strategy)
+
+
+def validate_scalar_strategy(name: str, strategy: st.SearchStrategy) -> None:
+    if not isinstance(name, str):
+        raise InvalidArgument(f"scalar name {name!r} must be a string")
+    if not isinstance(strategy, st.SearchStrategy):
+        raise InvalidArgument(
+            f"custom_scalars[{name!r}]={strategy!r} must be a Hypothesis "
+            "strategy which generates AST nodes matching this scalar."
+        )
